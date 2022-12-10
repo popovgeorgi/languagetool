67388a10a442ccb8164d6c385cfba96ae7afa1cb
popaqy <popovgeorgi002@gmail.com>
2022-12-10 20:30:39 +0200

    Make changes

--- languagetool-language-modules/bg/src/main/java/org/languagetool/language/Bulgarian.java
+++ languagetool-language-modules/bg/src/main/java/org/languagetool/language/Bulgarian.java
@@ -18,12 +18,25 @@
  */
 package org.languagetool.language;
 
-import org.languagetool.*;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.languagetool.Language;
+import org.languagetool.UserConfig;
+import org.languagetool.rules.*;
+import org.languagetool.rules.spelling.hunspell.HunspellNoSuggestionRule;
+import org.languagetool.tagging.Tagger;
+import org.languagetool.tagging.xx.DemoTagger;
+import org.languagetool.tokenizers.SRXSentenceTokenizer;
+import org.languagetool.tokenizers.SentenceTokenizer;
+import org.languagetool.rules.spelling.SpellingCheckRule;
+import org.languagetool.rules.spelling.hunspell.HunspellRule;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -32,12 +45,54 @@ import java.util.function.Function;
 
 import static java.util.Arrays.asList;
 
-/**
- * Support for Bulgarian
- * etc. if you need spell checking.
- * Make sure to call {@link #close()} after using this (currently only relevant if you make
- * use of {@link EnglishConfusionProbabilityRule}).
- */
-public class Bulgarian  {
+public class Bulgarian extends Language  {
+  @Override
+  public String getName() {
+    return "Bulgarian";
+  }
 
+  @Override
+  public String getShortCode() {
+    return "bg";
+  }
+
+  @Override
+  public String[] getCountries() {
+    return new String[]{"BG"};
+  }
+
+  @Override
+  public Contributor[] getMaintainers() {
+    return new Contributor[] {new Contributor("Georgi Popov")};
+  }
+
+  @NotNull
+  @Override
+  public Tagger createDefaultTagger() {
+    return new DemoTagger();
+  }
+
+  @Override
+  public SentenceTokenizer createDefaultSentenceTokenizer() {
+    return new SRXSentenceTokenizer(this);
+  }
+
+  @Override
+  public List<Rule> getRelevantRules(ResourceBundle messages, UserConfig userConfig, Language motherTongue, List<Language> altLanguages) throws IOException {
+    return Arrays.asList(
+            new CommaWhitespaceRule(messages),
+            new DoublePunctuationRule(messages),
+            new GenericUnpairedBracketsRule(messages),
+            new HunspellNoSuggestionRule(messages, this, userConfig, altLanguages),
+            new UppercaseSentenceStartRule(messages, this),
+            new WordRepeatRule(messages, this),
+            new MultipleWhitespaceRule(messages, this)
+    );
+  }
+
+  @Nullable
+  @Override
+  protected SpellingCheckRule createDefaultSpellingRule(ResourceBundle messages) throws IOException {
+    return new HunspellRule(messages, this, null,null);
+  }
 }
