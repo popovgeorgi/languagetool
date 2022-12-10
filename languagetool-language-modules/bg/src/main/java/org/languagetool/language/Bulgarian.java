27616443d9be85d09aaba74827885e399ace2ff4
popaqy <popovgeorgi002@gmail.com>
2022-12-10 14:46:24 +0200

    Add Bulgarian

---
+++ languagetool-language-modules/bg/src/main/java/org/languagetool/language/Bulgarian.java
@@ -0,0 +1,43 @@
+/* LanguageTool, a natural language style checker
+ * Copyright (C) 2007 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package org.languagetool.language;
+
+import org.languagetool.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+import static java.util.Arrays.asList;
+
+/**
+ * Support for Bulgarian
+ * etc. if you need spell checking.
+ * Make sure to call {@link #close()} after using this (currently only relevant if you make
+ * use of {@link EnglishConfusionProbabilityRule}).
+ */
+public class Bulgarian  {
+
+}
